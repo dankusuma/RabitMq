3ff632d54448259246bb4d6b217e60f075ee405a
Danang Kusuma Yudha <kusumayudha32@gmail.com>
2021-06-15 11:12:26 +0700

    Initial Commit

---
+++ Send/Send.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+       int i =0;
+        while(i==0)
+        {
+            Console.WriteLine("Masukan Pesan :");
+            string message = Console.ReadLine();
+            send(message);
+        }
+
+    }
+
+    public static void send(string message)
+    {
+        var factory = new ConnectionFactory() { HostName = "139.59.117.173" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+
+}
